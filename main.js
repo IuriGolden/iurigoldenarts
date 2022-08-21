2c1288ae63060f31230e2aba3a00768d74921959
IuriGolden <111701854+IuriGolden@users.noreply.github.com>
2022-08-21 15:13:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/* SCROLL ANIMAÇÃO SECTION 1*/
+sr.reveal('.section_1',{})
+sr.reveal('.container_1',{delay: 200})
+sr.reveal('.img-section_1', {delay: 200})
+sr.reveal('.h1_section_1', {delay: 400})
+sr.reveal('.p_section_1', {delay: 200})
+sr.reveal('.social-media', {interval: 200})
+
+
+/* SCROLL ANIMAÇÃO SECTION 2*/
+sr.reveal('.container_2',{delay: 200})
+
+
+
+/* SCROLL ANIMAÇÃO SECTION 3*/
+sr.reveal('.texts',{delay: 200})
+sr.reveal('.projetos-wrapper',{delay: 400})
+
